71947c629587af435bcd26e9cde424d4efe91b12
Yichen Xu <yichen.x@outlook.com>
2024-10-13 12:43:49 +0200

    cleanup

--- doom/config.el
+++ doom/config.el
@@ -155,65 +155,29 @@
       :leader
       "'" #'+vterm/toggle)
 
-;; ledger mode
-(use-package! ledger-mode
-  :mode ("\\.\\(ledger\\|journal\\|ldg\\)\\'" . ledger-mode)
-  :defer t
-
-  :init
-  (setq ledger-binary-path "hledger"
-        ledger-mode-should-check-version nil
-        ledger-report-links-in-register nil)
-
-  :config
-  (setq ledger-post-amount-alignment-column 62)
-  (setq-default ledger-reports
-                '(("monthly expenses" "%(binary) balance expenses --tree --no-total --row-total --average --monthly --begin '6 months ago'")
-                ("weekly expenses" "%(binary) balance expenses --tree --no-total --row-total --average --weekly --begin '4 weeks ago'")
-                ("weekly expenses (more)" "%(binary) balance expenses --tree --no-total --row-total --average --weekly --begin '10 weeks ago'")
-                ("daily expenses" "%(binary) balance expenses --tree --no-total --row-total --average --daily --begin '5 days ago'")
-                ("expenses this week" "%(binary) balance expenses --tree --no-total --row-total --average --daily --begin 'this week'")
-                ("expenses this month" "%(binary) balance expenses --tree --no-total --row-total --average --weekly --begin 'this month'")
-                ("sources this week" "%(binary) balance sources --tree --no-total --row-total --average --daily --begin 'this week'")
-                ("sources this month" "%(binary) balance sources --tree --no-total --row-total --average --weekly --begin 'this month'")
-                ("all assets" "%(binary) balance assets liabilities --tree")))
-  (setq-default ledger-report-auto-width nil)
-  (setq-default ledger-report-use-native-highlighting nil)
-
-  (map!
-   :map 'ledger-mode-map
-   :localleader
-   "q" #'ledger-post-align-xact
-   "r" #'ledger-report))
-
-(use-package! evil-ledger
-  :defer t
-  :init (add-hook 'ledger-mode-hook 'evil-ledger-mode))
-
+;; input method
 (use-package! agda-input)
 
 ;; gptel
 (use-package! gptel
   :defer t
   :config
-  (setq-default gptel-model "gpt-4o"))
+  (setq-default gptel-model "gpt-4o")
+  (map!
+   :map 'gptel-mode-map
+   :n "?" #'gptel-menu
+   :n "q" #'evil-quit)
+  (map!
+   :map 'gptel-mode-map
+   :localleader
+   "SPC" #'gptel-send
+   "a" #'gptel-add))
 ;; Set the api key in custom.el
 
 (map!
  :map 'override
  :leader
  :desc "Open gptel" "og" #'gptel)
-
-(map!
- :map 'gptel-mode-map
- :n "?" #'gptel-menu
- :n "q" #'evil-quit)
-
-(map!
- :map 'gptel-mode-map
- :localleader
- "SPC" #'gptel-send
- "a" #'gptel-add)
 
 ;; dotty
 (use-package! dotty
@@ -231,6 +195,7 @@
  :desc "Goto definition" "gg" #'lsp-ui-peek-find-definitions
  :desc "Goto references" "gr" #'lsp-ui-peek-find-references)
 
+;; zetteldeft
 (use-package! zetteldeft
   :after deft
   :config
