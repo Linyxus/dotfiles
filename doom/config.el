1ee4c3a90c9c3f6cacf4012f19c0e12498873bae
Yichen Xu <yichen.x@outlook.com>
2026-01-05 18:35:16 +0800

    Tweaks

--- doom/config.el
+++ doom/config.el
@@ -36,6 +36,8 @@
        :weight 'light))
 (setq-default line-spacing 10)
 
+(setq-default tab-width 2)
+
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
@@ -45,7 +47,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'standard-light)
+(setq doom-theme 'standard-light-tinted)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
